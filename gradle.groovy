4b2777c1787fb3fb04353df4344ff9d60fbe0f6a
ngamboa <ngamboa@insacom.cl>
2022-01-21 21:25:29 -0300

    update gradle.groovy

--- gradle.groovy
+++ gradle.groovy
@@ -17,24 +17,22 @@ def call() {
         sh 'curl -X GET http://localhost:8081/rest/mscovid/test?msg=testing'
     }
     stage('nexus') {
-        steps {
-            nexusPublisher nexusInstanceId: 'nexus3-docker',
-            nexusRepositoryId: 'ejemplo-gradle',
-            packages: [
-                [
-                    $class: 'MavenPackage',
-                    mavenAssetList: [
-                        [classifier: '', extension: '', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']
-                    ],
-                    mavenCoordinate: [
-                        artifactId: 'DevOpsUsach2020',
-                        groupId: 'com.devopsusach2020',
-                        packaging: 'jar',
-                        version: '0.0.1'
-                    ]
+        nexusPublisher nexusInstanceId: 'nexus3-docker',
+        nexusRepositoryId: 'ejemplo-gradle',
+        packages: [
+            [
+                $class: 'MavenPackage',
+                mavenAssetList: [
+                    [classifier: '', extension: '', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']
+                ],
+                mavenCoordinate: [
+                    artifactId: 'DevOpsUsach2020',
+                    groupId: 'com.devopsusach2020',
+                    packaging: 'jar',
+                    version: '0.0.1'
                 ]
             ]
-        }
+        ]
     }
 }
 
